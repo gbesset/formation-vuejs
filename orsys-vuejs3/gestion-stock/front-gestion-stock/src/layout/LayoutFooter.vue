c58f2ecda3487457663b39d297089dcde0e78385
guillaumeb <guillaume.besset@ponant-technologies.com>
2024-11-06 14:29:15 +0100

    Feat: footer

---
+++ orsys-vuejs3/gestion-stock/front-gestion-stock/src/layout/LayoutFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <a href="/">Mentions légales</a>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background: aliceblue;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    text-decoration: inherit;
+    color: inherit;
+  }
+  &:hover {
+    text-decoration: underline;
+  }
+}
+</style>
---
+++ orsys-vuejs3/gestion-stock/front-gestion-stock/src/layout/LayoutHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <a href="/"><img src="/cart.svg" alt="Logo" /><span>Gestion Stock</span></a>
+  </header>
+</template>
+
+<style scoped>
+img {
+  width: 30px;
+}
+header {
+  height: 130px;
+  background: aliceblue;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+}
+span {
+  font-weight: bold;
+  padding: 0 0.5rem;
+  font-size: 1.3rem;
+}
+a {
+  display: flex;
+  align-items: center;
+  text-decoration: inherit;
+  color: black;
+  gap: 0.5rem;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
