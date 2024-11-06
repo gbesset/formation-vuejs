c726bd0f18debec618bb2e089ea21fdb58c93730
guillaumeb <guillaume.besset@ponant-technologies.com>
2024-11-06 17:20:40 +0100

    Feat: list article

--- orsys-vuejs3/gestion-stock/front-gestion-stock/src/views/MentionLegalesView.vue
+++ orsys-vuejs3/gestion-stock/front-gestion-stock/src/views/MentionLegalesView.vue
@@ -1,12 +1,24 @@
 <template>
-  <section>
+  <main>
     <h1>Mentions légales</h1>
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis dolorem, at quas voluptatum
       assumenda sunt. Eos deserunt ratione, obcaecati consequatur quis suscipit iure iusto
       excepturi? Sit ullam totam voluptas deserunt?
     </p>
-  </section>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore nulla repellat consequatur
+      odio voluptatum eligendi accusamus laudantium illo est molestiae! Vero itaque, dignissimos
+      magni repudiandae, sapiente animi incidunt, ratione nam necessitatibus nemo dolore harum vel!
+      Sed, asperiores fugiat voluptate dicta minus distinctio blanditiis, laudantium, voluptas
+      repellat quas eos labore alias laboriosam numquam delectus itaque. Soluta placeat numquam
+      expedita facere explicabo repellat distinctio incidunt assumenda voluptates laudantium quo
+      totam perferendis tempora, libero maiores, sint accusamus eius, vitae illum voluptatem!
+      Similique perferendis nemo, neque modi ea, quibusdam nisi ipsa, eum fugiat nobis ducimus
+      cupiditate. Quos iusto fugit quod atque quibusdam et consequuntur!
+    </p>
+  </main>
 </template>
 
 <style></style>
