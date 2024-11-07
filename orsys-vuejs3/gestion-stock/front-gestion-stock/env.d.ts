347dee11f8b11fe130666065e0cdfc913b6c22ad
guillaumeb <guillaume.besset@ponant-technologies.com>
2024-11-07 10:06:18 +0100

    Feat: Typescript globalComponnt enrichissement

--- orsys-vuejs3/gestion-stock/front-gestion-stock/env.d.ts
+++ orsys-vuejs3/gestion-stock/front-gestion-stock/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+//Permet d'augmenter l'interface GlobalComponents qui est définie en y ajoutant le typage pour FaIcon
+declare module 'vue' {
+  export interface GlobalComponents {
+    FaIcon: typeof FontAwesomeIcon
+  }
+}
